6372f716702f44bedee50f0af3c6727b76dcd593
Aida Saifutdinova <75072925+hhhhmryan@users.noreply.github.com>
2021-10-12 15:24:13 +0300

    Add files via upload

---
+++ lab8/lab8/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Text;
+
+namespace lab8
+{
+    class Program
+    {
+        static StringBuilder task1(StringBuilder line)
+        {
+            for (int i = 0; i < line.Length - 1; i += 2)
+            {
+                char a = line[i];
+                line[i] = line[i + 1];
+                line[i + 1] = a;
+            }
+            return line;
+        }
+        static int task2(string letters, out int n)
+        {
+            n = 0;
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (char.IsLetter(letters[i]))
+                {
+                    n++;
+                }
+            }
+            return n;
+        }
+
+        static void task3(string mystring, out bool res)
+        {
+            res = false;
+            for (int i = 0; i < mystring.Length - 1; i++)
+            {
+                if (mystring[i] == mystring[i + 1])
+                {
+                    res = true;
+                }
+            }
+        }
+        static StringBuilder task4(StringBuilder middle)
+        {
+            if (middle.Length % 2 == 1)
+            {
+                middle.Remove(middle.Length / 2, 1);
+            }
+            else if (middle.Length % 2 == 0)
+            {
+                middle.Remove(middle.Length / 2 - 1, 2);
+            }
+            return middle;
+        }
+        static String task5(string sub)
+        {
+            Console.WriteLine("Введите подстроку, которую нужно заменить:");
+            string substring1 = Console.ReadLine();
+            Console.WriteLine("Введите подстроку, на которую нужно заменить исходную подстроку:");
+            string substring2 = Console.ReadLine();
+            sub = sub.Replace(substring1, substring2);
+            Console.WriteLine(sub);
+            return sub;
+        }
+        static int task6(string number, out int sum)
+        {
+            sum = 0;
+            for (int i = 0; i < number.Length; i++)
+                if (Char.IsDigit(number[i]))
+                {
+                    sum += Convert.ToInt32(Char.GetNumericValue(number[i]));
+                }
+            return sum;
+        }
+        static string task7(string cut)
+        {
+            int a = cut.IndexOf(':');
+            cut = cut.Substring(0, a);
+            Console.WriteLine(cut);
+            return cut;
+        }
+        static string task8(string cut)
+        {
+            int a = cut.LastIndexOf(':');
+            cut = cut.Substring(a + 1);
+            Console.WriteLine(cut);
+            return cut;
+        }
+        static string task9(string anotherone)
+        {
+            string[] cuts = anotherone.Split(',');
+            Console.WriteLine("Result: " + cuts[0] + cuts[cuts.Length - 1]);
+            return anotherone;
+        }
+
+        static string task10(string thestring, out int count)
+        {
+            count = 0;
+            for (int i=0; i< thestring.Length; i++)
+            {
+                int inner = 1;
+                for (int j = 0; j<i; j++)
+                {
+                    if (thestring[j]==thestring[i])
+                    {
+                        Console.WriteLine(thestring[j]+ " " + thestring[i]);
+                        inner = 0; ;
+                    }
+                }
+                if (inner!=0)
+                {
+                    count += inner;
+                }
+                Console.WriteLine("c=" + count);
+            }
+            return thestring;
+        }
+        static void Main(string[] args)
+        {   /*
+            //task1
+            StringBuilder line = new StringBuilder("Собака");
+            Console.WriteLine(line);
+            task1(line);
+            Console.WriteLine(line);
+
+            //task2
+            int n = 0;
+            Console.WriteLine("Введите строку:");
+            string letters = Console.ReadLine();
+            task2(letters, out n);
+            Console.WriteLine("Количество букв в строке: " + n);
+
+            //task3 
+            bool res;
+            Console.WriteLine("Введите строку:");
+            string mystring = Console.ReadLine();
+            task3(mystring, out res);
+            Console.WriteLine("Есть ли в строке два соседствующих одинаковых символа: " + res);
+
+            //task4
+            Console.WriteLine("Введите строку:");
+            StringBuilder middle = new StringBuilder(Console.ReadLine());
+            task4(middle);
+            Console.WriteLine(middle);
+
+            //task5
+            Console.WriteLine("Введите строку:");
+            string sub = Console.ReadLine();
+            task5(sub);
+
+            //task6
+            int sum;
+            Console.WriteLine("Введите строку:");
+            string number = Console.ReadLine();
+            task6(number, out sum);
+            Console.WriteLine("Сумма цифр в строке: " + sum);
+
+            //task7
+            Console.WriteLine("Введите строку:");
+            string cut = Console.ReadLine();
+            task7(cut);
+
+            //task8
+            Console.WriteLine("Введите строку:");
+            string cut1 = Console.ReadLine();
+            task8(cut1);
+
+            //task9
+            Console.WriteLine("Введите строку:");
+            string anotherone = Console.ReadLine();
+            task9(anotherone);
+            */
+            //task10
+            int count;
+            Console.WriteLine("Введите строку:");
+            string thestring = Console.ReadLine();
+            task10(thestring, out count);
+            Console.WriteLine("Различных символов: " + count);
+        }
+    }
+}
---
+++ lab9/lab9/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1
+            string line = "Дать знать, Если что-то не пОлучится";
+            Console.WriteLine("Введите слово для проверки: ");
+            string word = Console.ReadLine();
+            if (Regex.IsMatch(line, word))
+            {
+                Console.WriteLine("Ваше слово имеется в выражении.");
+            }
+            else Console.WriteLine("Вашего слова нет в выражени.");
+            Console.WriteLine();
+
+            //Task2
+            Console.WriteLine("Задайте длину слова: ");
+            int length = int.Parse(Console.ReadLine());
+            string pattern = @"\b(\w{" + length + @"})\b";
+            foreach (Match item in Regex.Matches(line, pattern))
+            {
+                Console.WriteLine(item.Value);
+            }
+            Console.WriteLine();
+
+            //Task3 
+            string newpatt = (@"\b[A-ZА-Я]\w*");
+            foreach (Match newmatch in Regex.Matches(line.ToString(), newpatt))
+            {
+                Console.WriteLine(newmatch.Value);
+            }
+            Console.WriteLine();
+
+            //Task4
+            string taskfour = "Why! :dogs?? are! so,,,, cute....";
+            string anotherpatt = (@"[,.!?;:]");
+            string replacement = "";
+            Console.WriteLine(Regex.Replace(taskfour, anotherpatt, replacement));
+            Console.WriteLine();
+
+            //Task5
+            string taskfive = "Привет my name is Кот, я was born в Казани. Мне ten (10) years с рождения.";
+            string pattern5 = (@"\b[A-Za-z]\w*");
+            string myrep = "...";
+            Console.WriteLine(Regex.Replace(taskfive, pattern5, myrep));
+
+            //Task6 
+            double sum = 0;
+            string tasksix = "good 4 / ? } 56 8 2,5 7 10 nice 0";
+            string pat = (@"[+-]?\d+\,?\d*");
+            foreach (Match summ in Regex.Matches(tasksix, pat))
+            {
+                sum = sum + double.Parse(summ.Value);
+            }
+            Console.WriteLine(sum);
+
+            //Task7
+            string taskseven = "15.11.1890 37.09.2020 11.02.1999 11.09.2017 17.09.2018 12.05.2001 04.05.2020 27.10.1996 26.03.1990 01.07.1995 31.05.2020";
+            string thisyear = (@"(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[012])\.2020");
+
+            foreach (Match info in Regex.Matches(taskseven, thisyear))
+            {
+                Console.WriteLine(info.Value);
+            }
+            Console.WriteLine();
+
+            //Task8
+            string taskeight = "15.11.1890 37.09.2020 11.02.1999 11.09.2017 17.09.2018 12.05.2001 04.05.2020 27.10.1996 26.03.1990 01.07.1995 31.05.2020";
+            Console.WriteLine(taskeight);
+            Console.WriteLine();
+            string date = (@"(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[12])\.(19[0-9]{2}|2020|200[0-9])");
+
+            foreach (Match thisday in Regex.Matches(taskeight, date))
+            {
+
+                Console.WriteLine("Дата: " + thisday.Value);
+
+                string[] parts = thisday.Value.Split('.');
+                int dd = int.Parse(parts[0]);
+                int mm = int.Parse(parts[1]);
+                int yy = int.Parse(parts[2]);
+
+                if (dd != 1)
+                {
+                    dd = dd - 1;
+                }
+                else if (dd == 1)
+                {
+                    if (mm != 1)
+                    {
+                        switch (mm)
+                        {
+                            case 2:
+                                mm = mm - 1;
+                                dd = 31;
+                                break;
+                            case 3:
+                                mm = mm - 1;
+                                dd = 28;
+                                break;
+                            case 4:
+                                mm = mm - 1;
+                                dd = 31;
+                                break;
+                            case 5:
+                                mm = mm - 1;
+                                dd = 30;
+                                break;
+                            case 6:
+                                mm = mm - 1;
+                                dd = 31;
+                                break;
+                            case 7:
+                                mm = mm - 1;
+                                dd = 30;
+                                break;
+                            case 8:
+                                mm = mm - 1;
+                                dd = 31;
+                                break;
+                            case 9:
+                                mm = mm - 1;
+                                dd = 31;
+                                break;
+                            case 10:
+                                mm = mm - 1;
+                                dd = 30;
+                                break;
+                            case 11:
+                                mm = mm - 1;
+                                dd = 31;
+                                break;
+                            case 12:
+                                mm = mm - 1;
+                                dd = 30;
+                                break;
+                        }
+                    }
+                    else if (mm == 1)
+                    {
+                        dd = 31;
+                        mm = 12;
+                        yy = yy - 1;
+                    }
+                }
+                string dd1 = dd.ToString();
+                string mm1 = mm.ToString();
+                string yy1 = yy.ToString();
+                string dot = ".";
+                string newdate = dd1 + dot + mm1 + dot + yy1;
+
+                Console.WriteLine("Дата прошлого дня: " + dd + "." + mm + "." + yy);
+
+
+                taskeight = Regex.Replace(taskeight, date, newdate);
+                Console.WriteLine(taskeight);
+                Console.WriteLine();
+            }
+            Console.WriteLine(taskeight);
+
+
+
+
+        }
+    }
+}
